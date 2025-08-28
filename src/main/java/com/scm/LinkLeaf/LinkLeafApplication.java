a207261cd49969d61902f5fb72778680e9504635
iamrautela <abhisheksinghrautela036@gmail.com>
2025-08-28 19:34:42 +0530

    added spring necsarry dependencies for project

---
+++ src/main/java/com/scm/LinkLeaf/LinkLeafApplication.java
@@ -0,0 +1,13 @@
+package com.scm.LinkLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkLeafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scm/LinkLeaf/LinkLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.LinkLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
