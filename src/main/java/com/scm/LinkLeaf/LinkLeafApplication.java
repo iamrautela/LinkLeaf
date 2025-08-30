c9f4c54e48480b8d76e976832c2c8417af443150
iamrautela <abhisheksinghrautela036@gmail.com>
2025-08-30 19:37:44 +0530

    Imported tailwind css in spring boot project

--- src/main/java/com/scm/LinkLeaf/LinkLeafApplication.java
+++ src/main/java/com/scm/LinkLeaf/LinkLeafApplication.java
@@ -1,13 +1,12 @@
 package com.scm.LinkLeaf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class LinkLeafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinkLeafApplication.class, args);
 	}
+
 
 }
